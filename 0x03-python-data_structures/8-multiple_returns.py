a8e5294278a75ff859317020d4499cbed7f86865
AerinLom <aerinlom1@gmail.com>
2024-01-07 03:13:51 -0800

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-     if not sentence:
-         sentence = None
+    if not sentence:
+        return (0, None)
 
-     if sentence:
-         num_char = len(sentence)
-
-     else:
-         num_char = 0
-     
-     return (num_char, sentence if not sentence else sentence[:1])     
+    return (len(sentence), sentence[0])    
