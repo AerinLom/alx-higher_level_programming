2c8ddf60fcd3c17d1c27e14e34656040779fc212
AerinLom <aerinlom1@gmail.com>
2024-01-07 03:52:21 -0800

    Task 8, trying to fix requirement

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if not sentence:
-        sentence = None
+    if len(sentence) == 0:
+        return 0, None
 
-    if sentence:
+    elif len(sentence) > 0:
         char_len = len(sentence)
-
-    else:
-        char_len = 0
-
-    return (char_len, sentence if not sentence else sentence[:1])    
+        return char_len, sentence[0]    
