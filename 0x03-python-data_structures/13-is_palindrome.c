404d75a47a261482db88e95461f01f3b7ff01497
AerinLom <aerinlom1@gmail.com>
2024-01-07 12:08:42 -0800

    Task 13 fixed

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,32 +8,22 @@
 
 int is_palindrome(listint_t **head)
 {
-listint_t *reversed_list = NULL;
-listint_t *current_head = *head;
-listint_t *copied_list = NULL;
-listint_t *copied_list_ptr = NULL;
+listint_t *reversed_list = NULL, *current_head = *head;
+listint_t *copied_list = NULL, *copied_list_ptr = NULL;
 
 if (*head == NULL || current_head->next == NULL)
 	return (1);
 
 while (current_head != NULL)
 {
-	listint_t *new_node = malloc(sizeof(listint_t));
-
-	if (new_node == NULL)
-	{
-		free_listint(reversed_list);
-		return (0);
-	}
-	new_node->n = current_head->n;
-	new_node->next = reversed_list;
-	reversed_list = new_node;
-
+	add_nodeint_end(&reversed_list, current_head->n);
 	add_nodeint_end(&copied_list, current_head->n);
 	current_head = current_head->next;
-	copied_list_ptr = copied_list;
 }
-while (*head != NULL && copied_list_ptr != NULL)
+
+copied_list_ptr = copied_list;
+
+while (*head != NULL)
 {
 	if ((*head)->n != copied_list_ptr->n)
 	{
@@ -41,10 +31,13 @@ while (*head != NULL && copied_list_ptr != NULL)
 		free_listint(copied_list);
 		return (0);
 	}
+
 *head = (*head)->next;
 copied_list_ptr = copied_list_ptr->next;
 }
+
 free_listint(reversed_list);
 free_listint(copied_list);
-return (0);
+
+return (1);
 }
