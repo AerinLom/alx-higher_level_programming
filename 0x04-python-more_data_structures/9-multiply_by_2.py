89ab55720cc7598b9bb1fbcb6c0c33f376b66c6a
AerinLom <aerinlom1@gmail.com>
2024-01-10 05:20:26 -0800

    Task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,7 +4,7 @@ def multiply_by_2(a_dictionary):
 
     multiplied_dictionary = dict(a_dictionary)
 
-    for key, values in multiplied_dictionary:
-        multiplied_dictionary[key] = 2 * values
+    for key, values in multiplied_dictionary.items():
+        multiplied_dictionary[key] = values * 2
 
     return multiplied_dictionary
