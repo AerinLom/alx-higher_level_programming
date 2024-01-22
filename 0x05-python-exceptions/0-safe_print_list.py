7b4fad26a1148d633ca090406e329539ffe994dd
AerinLom <aerinlom1@gmail.com>
2024-01-22 04:44:14 -0800

    Task 0, trying to solve checks

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,7 @@ def safe_print_list(my_list=[], x=0):
         for index in range(x):		
             print("{}".format(my_list[index]), end="")
             counter += 1
-    except (IndexError, TypeError):
+    except IndexError:
         pass
-    finally:
-        print("")
+    print("")
     return counter
