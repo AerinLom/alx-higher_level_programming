36f267485792d2483899ce1b2456cc7100b367cd
AerinLom <aerinlom1@gmail.com>
2024-01-23 04:18:03 -0800

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Defines a square based on 0-square.py.
+"""
+
+
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+        """
+        self.__size = size
