7c0a5590db4b19087b050abc59358b57c521f386
AerinLom <aerinlom1@gmail.com>
2024-02-05 03:05:35 -0800

    Task 1, fixed the documentation

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,9 +5,12 @@ This module inherits from list
 
 
 class MyList(list):
+    """
+    Conducts a sorted sub class list to be printed
+    """
     def print_sorted(self):
         """
         Prints a list in ascending order
         """
-        sorted_list = sorted(self)
-        print(sorted_list)
+	list_sorted = sorted(self)
+        print(list_sorted)
