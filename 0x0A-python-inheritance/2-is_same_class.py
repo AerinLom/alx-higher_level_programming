16890a2611283b5489c26988771a894698e4f023
AerinLom <aerinlom1@gmail.com>
2024-02-05 02:37:42 -0800

    Fixed task 2 & 3 checks, testing issue of task 1

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,6 +3,7 @@
 This module defines a object validity checker
 """
 
+
 def is_same_class(obj, a_class):
     """
     Checks for an instance of a_class
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,6 +3,7 @@
 This module defines an object is an instance of a class checker
 """
 
+
 def is_kind_of_class(obj, a_class):
     """
     This checks if object is an instance of a class
