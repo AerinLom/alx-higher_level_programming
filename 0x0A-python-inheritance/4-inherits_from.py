ddec5dc2c61bd3484d2af0155c341ad1fb997b89
AerinLom <aerinlom1@gmail.com>
2024-02-05 03:18:11 -0800

    Task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,6 +8,6 @@ def inherits_from(obj, a_class):
     """
     Check if object is an instance of a class that inherited from the class
     """
-    if isinstance(obj, type) and issubclass(type(obj), a_class)
+    if isinstance(obj, type) and issubclass(type(obj), a_class):
         return True
     return False
