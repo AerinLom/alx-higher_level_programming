a7de5b9246edf32cfb0f51b849abb5866bcac660
AerinLom <aerinlom1@gmail.com>
2024-02-06 05:55:54 -0800

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Defines a class student
+"""
+
+
+class Student:
+    """
+    Attributes of a student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        
+        result = {}
+        for attribute in sorted(attrs):
+            if hasattr(self, attribute):
+                result[attribute] = getattr(self, attribute)
+        return result
