f9e6a8f4bbc368a0c5537f3805a64ccc666e48fe
AerinLom <aerinlom1@gmail.com>
2024-02-12 10:59:30 -0800

    Trying to find requirement fail

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
 """Defines a class Rectangle that inherits from Base"""
-from models.base import Base
+BaseModule = __import__('models.base')
+Base = BaseModule.Base
 
 
 class Rectangle(Base):
