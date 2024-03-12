c223af1cd649809c75843b7ae2bce98e2bb86c35
AerinLom <aerinlom1@gmail.com>
2024-03-12 06:21:03 -0700

    Task 15: Group by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,7 +1,6 @@
 -- lists the number of records with the same score in table
 -- second_table of database hbtn_0c_0 in MySQL server.
-SELECT Score, COUNT(*)
-AS number
+SELECT Score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
 ORDER BY number DESC;
