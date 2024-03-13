5af4a5d2e1198b32ad0dba070a0e824d7b4a9b87
AerinLom <aerinlom1@gmail.com>
2024-03-13 05:10:00 -0700

    Task 8: Show California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,8 @@
 -- lists all cities of California that can be found in database hbtn_0d_usa
 SELECT id, name
 FROM cities
-WHERE name = California
+WHERE state_id = (
+SELECT id
+FROM states
+WHERE name = California)
 ORDER BY id ASC;
