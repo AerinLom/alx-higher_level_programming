687e0ffda8e2a1d621b995176f6715dd12a1cd55
Aerin Lombard <aerinlom1@gmail.com>
2024-04-08 04:48:04 -0700

    Task 7: C is fun, x times

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,12 +3,12 @@
 const printNum = process.argv[2];
 const occur = parseInt(printNum, 10);
 const phrase = 'C is fun';
-let count = 1;
+let count = 0;
 
-if (!occur) {
+if (isNaN(occur)) {
   console.log('Missing number of occurrences');
 } else {
-  while (count !== printNum) {
+  while (count < occur) {
     console.log(phrase);
     count++;
   }
