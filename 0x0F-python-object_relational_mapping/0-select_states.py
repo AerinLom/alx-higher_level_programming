f4e5eb79490e3fec131577837da81c03521956b5
AerinLom <aerinlom1@gmail.com>
2024-04-11 03:47:30 -0700

    Task 0: Get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3],
+        port=3306
+    )
+    cursor = database.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    cursor.close()
+    database.close()
+
