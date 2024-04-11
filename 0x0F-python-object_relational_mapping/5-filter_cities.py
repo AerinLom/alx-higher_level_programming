5da50ad9a2e562cab9b7844a4d7246496cb6f88a
AerinLom <aerinlom1@gmail.com>
2024-04-11 05:56:02 -0700

    Task 5: Cities by state keyword

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Lists all cities for each state from the database hbtn_0e_4_usa"""
+import MySQLdb
+import sys
+
+state = sys.argv[4]
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3],
+        port=3306
+    )
+    cursor = database.cursor()
+    cursor.execute("SELECT cities.name FROM cities INNER JOIN "
+                   "states ON states.id=cities.state_id WHERE states.name"
+                   " LIKE %s", (state, ))
+    results = cursor.fetchall()
+    cities = ' '.join(row[0] for row in results)
+    print(cities)
+    cursor.close()
+    database.close()
