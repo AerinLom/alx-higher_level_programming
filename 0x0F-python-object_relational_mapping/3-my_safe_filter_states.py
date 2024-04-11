d6c9cb746aff2ac5cacf2f37868e4d77d6205822
AerinLom <aerinlom1@gmail.com>
2024-04-11 04:27:29 -0700

    Task 3: Injection secure

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+state = sys.argv[4]
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3],
+        port=3306
+    )
+    cursor = database.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s", (state, ))
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    cursor.close()
+    database.close()
