606d08680df68896ee9baecd15c549424f8456b8
AerinLom <aerinlom1@gmail.com>
2024-04-25 05:54:20 -0700

    Task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,5 +6,5 @@ Algorithm to find peaK in list of integers
 def find_peak(list_of_integers):
     """Finds a peak in a list of unsorted integers."""
     if list_of_integers:
-        sorted_list = sorted(list_of_integers, reverse=True)
-        return sorted_list[0] 
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
