8cd3cda44f1bb52a9bbe58b75fab4763f8746a8a
AerinLom <aerinlom1@gmail.com>
2024-04-26 07:29:13 -0700

    Task 1: Response from header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,9 +2,11 @@
 """
 A script that takes input url and displays the value of X-Request-Id variable
 """
-import urllib.request
-import sys
 
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.headers["X-Request-Id"])
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers["X-Request-Id"])
