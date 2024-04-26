9a543b6f1c1902fa5df62ed32b72cd0225dcd12f
AerinLom <aerinlom1@gmail.com>
2024-04-26 07:42:38 -0700

    task 2: email

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,5 +8,7 @@ if __name__ == "__main__":
     import sys
     import urllib.request
 
-    with urllib.request.urlopen(sys.argv[1]) as response:
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
         print(response.headers["X-Request-Id"])
